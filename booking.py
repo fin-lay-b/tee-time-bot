f38ce2af140bd29a00a3978439e7b7e69bc17db4
Finlay <finlay.brierton@dnv.com>
2025-05-27 21:32:33 +0100

    Refactor WCGCAuth class to use base URL for login and enhance error handling in login method

--- booking.py
+++ booking.py
@@ -5,12 +5,33 @@ from wc_gc.config import (
     WCGC_CONSENT_ENDPOINT,
 )
 
+# inject session
+# session = requests.Session()
+# session.verify = ...
+
 
 # Class to handle login and authentication
 class WCGCAuth:
     def __init__(self, session: requests.Session):
         self.session = session
-        self.login_url = "https://www.westcoastgolfclub.com.au/member/login"
+        self.login_url = f"{WCGC_BASE_URL}"
+
+    def login(self, memberid: str, pin: str):
+        try:
+            login_response = self.session.post(
+                self.login_url,
+                data={"memberid": memberid, "pin": pin},
+            )
+            login_response.raise_for_status()
+
+            return login_response
+
+        except requests.exceptions.RequestException as e:
+            print(f"[login] Failed: {e}")
+
+    def verify_login(self):
+        # Check the reponse for successful login
+        pass
 
 
 # Class to handle the interpreting of target tee times
