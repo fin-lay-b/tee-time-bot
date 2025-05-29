d0b6958a5e4b957e2555eced4ba62313e37da6da
Finlay <finlay.brierton@dnv.com>
2025-05-29 20:46:08 +0100

    Refactor WCLogin class to include booking page and consent URLs; add load_booking_page method for enhanced booking flow

--- booking.py
+++ booking.py
@@ -11,10 +11,12 @@ from wc_gc.config import (
 
 
 # Class to handle login and authentication
-class WCGCAuth:
+class WCLogin:
     def __init__(self, session: requests.Session):
         self.session = session
         self.login_url = f"{WCGC_BASE_URL}"
+        self.booking_page_url = f"{WCGC_BASE_URL}{WCGC_BOOKING_PAGE_ENDPOINT}"
+        self.consent_url = f"{WCGC_BASE_URL}{WCGC_CONSENT_ENDPOINT}"
 
     def login(self, memberid: str, pin: str):
         try:
@@ -29,16 +31,42 @@ class WCGCAuth:
         except requests.exceptions.RequestException as e:
             print(f"[login] Failed: {e}")
 
+    def load_booking_page(self):
+        try:
+            # Load the booking page
+            booking_page_response = self.session.get(self.booking_page_url)
+            # Check for errors in the booking page response
+            booking_page_response.raise_for_status()
+
+            # Accept conduct terms to proceed to booking page
+            conduct_response = self.session.get(self.consent_url, allow_redirects=True)
+            # Check for errors in the conduct response
+            conduct_response.raise_for_status()
+
+            return conduct_response
+
+        except requests.exceptions.RequestException as e:
+            print(f"[load_booking_page] Failed: {e}")
+
     def verify_login(self):
         # Check the reponse for successful login
         pass
 
 
 # Class to handle the interpreting of target tee times
-class TeeTimes:
+class TeeTimeManager:
+    # Need to be ablet to check availability of tee times (competiions etc.)
+    # Take target time for day of week
+    # Take target time for specific date
+    # Book best time in range of times
     pass
 
 
 # Class to handle the booking functionality
-class BookingSystem:
+class TeeTimeBooker:
+    def __init__(self, session: requests.Session, booking_date: str, booking_time: str):
+        self.session = session
+        self.booking_date = booking_date
+        self.booking_time = booking_time
+
     pass
